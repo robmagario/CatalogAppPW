fd9074df3d6dd194c2d10234442513abd4437457
robmagario <robson@magario.com>
2015-09-19 15:31:01 +0800

    Commited Kenta' version that introduces the concept of tables.

--- site/templates/section.php
+++ site/templates/section.php
@@ -6,10 +6,15 @@ $output = null;
 $header = Rest\Header::mimeType('json');
 
 if (Rest\Request::is('get')) {
-	$output = array();
-	$output['title'] = $page->title;
-	$output['page'] = $page->page_number;
-	$output['content'] = $page->content;
+        $output = array();
+        $output['title'] = $page->title;
+        $output['diagram'] = $page->section_diagram->first()->url;
+        $output['headerImage'] = $page->images->first()->url;
+        $tables = array();
+        foreach ($page->table_repeater as $table) {
+                array_push($tables, $table->content);
+        }
+        $output['tables'] = $tables;
 }
 
 http_response_code($code);
